1cc26084e7cece3e2bcd21bfc6a716c48b8022d5
itimor <itimor@techdog.com>
2017-10-13 11:44:55 +0800

    add group

--- reports/serializers.py
+++ reports/serializers.py
@@ -3,12 +3,15 @@
 
 from reports.models import WeekReport
 from rest_framework import serializers
+from users.models import Group
 
 class WeekReportSerializer(serializers.ModelSerializer):
     owner = serializers.SerializerMethodField(read_only=True)
+    group = serializers.SlugRelatedField(queryset=Group.objects.all(), slug_field='name')
     class Meta:
         model = WeekReport
-        fields = ('url', 'title', 'content', 'owner', 'week_count', 'date')
+        fields = ('url', 'title', 'content', 'owner', 'group', 'week_count', 'date')
+        read_only_fields = ('group',)
 
     def get_owner(self, obj):
         return obj.owner.name
--- reports/views.py
+++ reports/views.py
@@ -4,10 +4,12 @@
 from reports.models import WeekReport
 from rest_framework import viewsets
 from reports.serializers import WeekReportSerializer
+from users.models import User
 
 class WeekReportViewSet(viewsets.ModelViewSet):
     queryset = WeekReport.objects.all().order_by('-date')
     serializer_class = WeekReportSerializer
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        userinfo = User.objects.get(username=self.request.user)
+        serializer.save(owner=self.request.user, group=userinfo.group)
