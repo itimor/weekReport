f25eb4bb876ce803777ee6f3cbbf8e105cfbbb50
itimor <itimor@techdog.com>
2017-10-13 17:39:29 +0800

    fix filter

--- reports/filters.py
+++ reports/filters.py
@@ -4,18 +4,16 @@
 from django.db.models import Q
 from dry_rest_permissions.generics import DRYPermissionFiltersBase
 from users.models import User
+from django.shortcuts import get_object_or_404
 
 class ReportFilterBackend(DRYPermissionFiltersBase):
     def filter_list_queryset(self, request, queryset, view):
-        try:
-            userinfo = User.objects.get(username=request.user)
-            role = userinfo.roles.name
-            group = userinfo.group
-            if role == 'admin':
-                return queryset.all()
-            elif role == 'groupadmin':
-                return queryset.filter(Q(group=group))
-            else:
-                return queryset.filter(Q(owner=request.user))
-        except:
-            pass
+        userinfo = get_object_or_404(User, username=request.user)
+        role = userinfo.roles.name
+        group = userinfo.group
+        if request.user == 'admin' or role == 'admin':
+            return queryset.all()
+        elif role == 'groupadmin':
+            return queryset.filter(Q(group=group))
+        else:
+            return queryset.filter(Q(owner=request.user))
